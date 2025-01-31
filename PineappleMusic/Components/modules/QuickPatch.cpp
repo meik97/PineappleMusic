c1bd4599e3e27cdfb0dd025a5b836bed37e14df1
meik97 <meik97>
2025-01-31 22:07:37 +0100

    enable AMP service debug log instead of amp library log

--- PineappleMusic/Components/modules/QuickPatch.cpp
+++ PineappleMusic/Components/modules/QuickPatch.cpp
@@ -104,13 +104,17 @@ namespace Modules
 
 	QuickPatch::QuickPatch()
 	{
-		//AMPServices debug log
-		Utils::Hook::InstallHook((void*)(AMPLibraryAgentBase + 0x0200A44), print_f);
-		
-		Utils::Hook::InstallHook((void*)(AMPLibraryAgentBase + 0x0A1A614), calculateVolume); //we dont need this
+		//AMP library agent debug log
+		//Utils::Hook::InstallHook((void*)(AMPLibraryAgentBase + 0x0200A44), print_f);
+
+		//we dont need this
+		//Utils::Hook::InstallHook((void*)(AMPLibraryAgentBase + 0x0A1A614), calculateVolume); 
 
 		//Volume step frequency
-		//Utils::Hook::InstallHook((void*)(MicrosoftUIXamlBase + 0xFEF6), fixVolumeStepFrequency);
+		Utils::Hook::InstallHook((void*)(MicrosoftUIXamlBase + 0xFEF6), fixVolumeStepFrequency);
+
+		//AMP Services debug log
+		Utils::Hook::InstallHook((void*)(AMPServicesBase + 0x011D18C), print_f);
 	}
 
 }
